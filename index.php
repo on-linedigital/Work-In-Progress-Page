84d2b9e38063f16bf4395ea3bc2db16d95e1e13b
Marshall Woods <80414685+FrontlineGenisis@users.noreply.github.com>
2022-07-04 21:15:42 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+  <head>
+    <title>Site Unavailable | Genesis</title>
+    
+    <!-- SEO -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:url" content="/">
+    <meta name="twitter:title" content="Site Unavailable | Genesis">
+    <meta name="twitter:description" content="Response Code: 503, Site Unavailable.">
+    <meta name="twitter:image" content="https://Work-in-progress.genisis-mgmt.repl.co/assets/img/503.png">
+    <meta name="twitter:site" content="@">
+    <meta name="twitter:creator" content="@genesistechpeople">
+    <meta property="og:title" content="Site Unavailable | Genesis"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:url" content="/"/>
+    <meta property="og:image" content="https://Work-in-progress.genisis-mgmt.repl.co/assets/img/503.png"/>
+    <meta property="og:description" content="Response Code: 503, Site Unavailable."/>
+    
+    <!-- Import CSS -->
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/fonts/fonts.css">
+    
+  </head>
+  <body>
+  <div class="content">
+<p class="logo">{G}</p>
+<p class="text">This site is currently in the works by Genesis. Site is expected to be finised in T-Minus:</p><p id="cntdwn"></p>
+  </div>
+    
+    <!-- Importing JS -->
+    <script type="text/javascript" src="./assets/js/countdown.js"></script>
+
+  </body>
+</html>
