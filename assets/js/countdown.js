a0dea9aa5dc18ddc6399c361705fb78de7a850d3
Devalappar <>
2022-07-05 13:45:08 +0000

    made code look noice

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,15 +1,15 @@
-        var countDownDate = new Date("Jul 4, 2022 22:33:0").getTime();
-        var x = setInterval(function() {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("cntdwn").innerHTML = days + "d " + hours + "h "
-          + minutes + "m " + seconds + "s ";
-          if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("counter").innerHTML = "0d 0h 0m 0s";
-          }
-        }, 1000);
+var countDownDate = new Date("Jul 4, 2022 22:33:0").getTime();
+var x = setInterval(function () {
+   var now = new Date().getTime();
+   var distance = countDownDate - now;
+   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+   document.getElementById("cntdwn").innerHTML = days + "d " + hours + "h " +
+      minutes + "m " + seconds + "s ";
+   if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("counter").innerHTML = "0d 0h 0m 0s";
+   }
+}, 1000);
