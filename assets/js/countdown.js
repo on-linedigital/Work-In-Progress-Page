d78a0b33caf14f2c364aee7f79d168580bf4ef4d
Frontline Genisis <>
2022-07-05 15:38:59 +0000

    idk tbh

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,15 +1,1 @@
-var countDownDate = new Date("Jul 5, 2200 0:0:0").getTime();
-var x = setInterval(function () {
-   var now = new Date().getTime();
-   var distance = countDownDate - now;
-   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-   document.getElementById("cntdwn").innerHTML = days + "d " + hours + "h " +
-      minutes + "m " + seconds + "s ";
-   if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("counter").innerHTML = "0d 0h 0m 0s";
-   }
-}, 1000);
+var countDownDate=new Date("Jul 5, 2200 0:0:0").getTime(),x=setInterval(function(){var e=(new Date).getTime(),t=countDownDate-e,n=Math.floor(t/864e5),o=Math.floor(t%864e5/36e5),a=Math.floor(t%36e5/6e4),e=Math.floor(t%6e4/1e3);document.getElementById("cntdwn").innerHTML=n+"d "+o+"h "+a+"m "+e+"s ",t<0&&(clearInterval(x),document.getElementById("counter").innerHTML="0d 0h 0m 0s")},1e3);
